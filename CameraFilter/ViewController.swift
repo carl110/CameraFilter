594c82634e24ff97531da8c2dd4db5dd946a715b
Carl Wainwright <carljame7@gmail.com>
2019-11-28 13:15:43 +0000

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Carl Wainwright on 28/11/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
